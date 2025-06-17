740b6ad986a2bd3097ceeadef8ae0f344acde061
LeoDreamer <2200010825@stu.pku.edu.cn>
2025-06-17 21:29:44 +0800

    feat: group styles

--- src/cssImport.ts
+++ src/cssImport.ts
@@ -1,8 +1,20 @@
 const url = window.location.href;
 
-if (/^http:\/\/openjudge.cn/.test(url)) {
+if (url == "http://openjudge.cn/") {
     import('./css/index.css').then(() => {
-        console.log(url);
         console.log('openjudge index styles loaded');
+    });
+} else if (/^http:\/\/.*\.openjudge\.cn\/$/.test(url)) {
+    import('./css/group.css').then(() => {
+        console.log('openjudge group styles loaded');
+    });
+} else if (/^http:\/\/.*\.openjudge\.cn\/practice\/.*/.test(url)) {
+    import('./css/practice.css').then(() => {
+        console.log('openjudge practice styles loaded');
+    });
+} else {
+    console.log(url);
+    import('./css/global.css').then(() => {
+        console.log('openjudge common styles loaded');
     });
 }
